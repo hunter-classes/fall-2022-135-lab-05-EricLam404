a7eef57b2d3ed4d7bb926ca1e5b558073dd5a620
EricLam404 <lameric9888@gmail.com>
2022-10-03 09:59:18 -0400

    Added functions to main

--- main.cpp
+++ main.cpp
@@ -10,5 +10,35 @@ int main()
   std::cout << std::boolalpha << isDivisibleBy(35, 17) << "\n";
   std::cout << "\n--------------\n";
 
+  std::cout << "Is a prime? \n";
+  std::cout << "isPrime(11)\n";
+  std::cout << isPrime(11) << "\n";
+  std::cout << "\n--------------\n";
+
+  std::cout << "Next prime? \n";
+  std::cout << "nextPrime(14)\n";
+  std::cout << nextPrime(14) << "\n";
+  std::cout << "\n--------------\n";
+
+  std::cout << "Count prime? \n";
+  std::cout << "countPrimes(11, 17)\n";
+  std::cout << countPrimes(11, 17) << "\n";
+  std::cout << "\n--------------\n";
+
+  std::cout << "Is twin prime? \n";
+  std::cout << "isTwinPrime(13)\n";
+  std::cout << isTwinPrime(13) << "\n";
+  std::cout << "\n--------------\n";
+
+  std::cout << "Next twin prime? \n";
+  std::cout << "nextTwinPrime(13)\n";
+  std::cout << nextTwinPrime(13) << "\n";
+  std::cout << "\n--------------\n";
+
+  std::cout << "Largest twin prime? \n";
+  std::cout << "largestTwinPrime(13)\n";
+  std::cout << largestTwinPrime(5, 18) << "\n";
+  std::cout << "\n--------------\n";
+
   return 0;
 }
